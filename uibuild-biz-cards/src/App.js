a278305508747c51acd1bc75c87dfd7a46d2adfe
10Cdegree <armin.szathmari@outlook.com>
2022-02-08 11:43:53 +0100

    Added stuff

---
+++ uibuild-biz-cards/src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+
+function App() {
+  return (
+    <body>
+      <div className="div1">
+        <elements>
+          <h2>Name1</h2>
+          <h6>RestaurantName1</h6>
+          <p>Address1</p>
+        </elements>
+      </div>
+      <div className="div2">
+        <elements>
+          <h2>Name2</h2>
+          <h6>RestaurantName1</h6>
+          <p>Address1</p>
+        </elements>
+      </div>
+      <div className="div3">
+        <elements>
+          <h2>Name3</h2>
+          <h6>RestaurantName1</h6>
+          <p>Address1</p>
+        </elements>
+      </div>
+    </body>
+  );
+}
+
+export default App;
